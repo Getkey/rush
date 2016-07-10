18903d0e0d955bbf60cc92017f3b43d7e626f887
Getkey <getkey@gmx.fr>
2016-07-11 00:30:38 +0200

    Use an actual tokenizer
    It makes the task much easier as separating string and generating the tree can be done independently.

--- src/interpret.rs
+++ src/interpret.rs
@@ -1,61 +1,140 @@
 extern crate builtins;
 
-use std::str::Chars;
-
 
 pub fn read(command_line: &str) {
-	match control_machine(&mut command_line.chars()) {
+	/*match control_machine(command_line) {
 		Ok(mut tree) => tree.execute_command(),
 		Err(err) => println!("{}", err),
-	}
-
+	}*/
+	let token_list = tokenize(command_line);
+	let token_iter = token_list.iter();
 }
 
-#[derive(PartialEq, Copy, Clone)] // this enum takes 1B therefore copying it is better than having a 4B pointer (on 32bit machines) or a 8B pointer (on 64 bit machines)
+#[derive(PartialEq, Copy, Clone)] // this enum takes 8b therefore copying it is better than having a 32b or 64b pointer
 pub enum State {
 	Start,
-	StrArg,
-	SubcommandEnd,
+	StrCmd(usize),
+	SeparateWs,
+	StrArg(usize),
 }
 
-fn tokenize(char_it: &mut Chars) -> (Result<Command, &'static str>, State) { // uses a recursive automaton
+pub enum Token<'a, 'b> {
+	LeftParen,
+	RightParen,
+	Cmd(&'a str),
+	Arg(&'b str),
+}
+
+fn tokenize(command_line: &str) -> Vec<Token> {
 	use self::State::*;
+	use self::Token::*;
+
+	let mut state = Start;
+	let mut token_list = Vec::new();
+
+	let mut char_it = command_line.char_indices();
+
+	loop {
+		if let Some((i, curr_char)) = char_it.next() {
+			match (state, curr_char) {
+				(Start, ' ') | (Start, '\t') => {},
+				(Start, '(') => token_list.push(LeftParen),
+				(Start, _) => state = StrCmd(i),
+
+				(StrCmd(slice_start), ')') => {
+					token_list.push(Cmd(&command_line[slice_start..i]));
+					state = Start;
+				},
+				(StrCmd(slice_start), ' ') | (StrCmd(slice_start), '\t') => {
+					token_list.push(Cmd(&command_line[slice_start..i]));
+					state = SeparateWs;
+				},
+				(StrCmd(_), _) => {},
+
+				(SeparateWs, ' ') | (SeparateWs, '\t') => {},
+				(SeparateWs, ')') => {
+					token_list.push(RightParen);
+					state = Start;
+				},
+				(SeparateWs, '(') => {
+					token_list.push(LeftParen);
+					state = Start;
+				},
+				(SeparateWs, _) => state = StrArg(i),
+
+				(StrArg(slice_start), ' ') | (StrArg(slice_start), '\t') => {
+					token_list.push(Arg(&command_line[slice_start..i]));
+					state = SeparateWs;
+				},
+				(StrArg(slice_start), ')') => {
+					token_list.push(Arg(&command_line[slice_start..i]));
+					state = Start;
+				}
+				(StrArg(_), _) => {},
+			}
+		}
+	}
+}
+
+use std::slice::Iter;
+
+fn generate_tree<'a>(token_list: Iter<Vec<Token>>) -> Result<Command<'a>, &'static str> {
+	Err("Empy commands are not allowed")
+	/*use self::State::*;
 	let mut cmd = Command::new();
 
 	let mut state = State::Start;
+	let slice_start = 0; // whatever, doesn't matter
+	let oldI = 0;
 
 	loop {
-		if let Some(curr_char) = char_it.next() {
+		if let Some((i, curr_char)) = char_it.next() {
 			match (state, curr_char) {
-				(Start, ')') => {
-					state = SubcommandEnd;
-					return (Ok(cmd), state);
-				},
+				(Start, ')') => return (Err("Empty subcommands are not allowed"), state),
 				(Start, ' ') | (Start, '\t') => {},
 				(Start, '(') => {
 					// do recursion
-					let (res, recurState) = tokenize(char_it);
-					if recurState != SubcommandEnd {
+					let (res, recur_state) = tokenize(char_it);
+					if recur_state != SubcommandEnd {
 						panic!("ERROR");
 					} else if let Err(_) = res {
 						return (res, state);
+					} else {
+						cmd.cmd.push(Param::Cmd(res));
 					}
 				},
 				(Start, _) => {
-					state = StrArg
-					// create string
-					// push character
+					state = StrCmd;
+					slice_start = i;
 				},
 
-				(StrArg, ')') => {
+				(StrCmd, ')') | (SeparateWs, ')') | (StrArg, ')') => {
 					state = SubcommandEnd;
 					return (Ok(cmd), state);
 				},
+
+				(StrCmd, ' ') | (StrCmd, '\t') => {
+				//	cmd.cmd = &
+				},
+				(StrCmd, _) => {},
+
+				(SeparateWs, ' ') | (SeparateWs, '\t') => {},
+				(SeparateWs, _) => {
+					state = StrArg;
+					cmd.params.push(Param::Arg(curr_char.to_string()));
+				},
+
 				(StrArg, ' ') | (StrArg, '\t') => {
-					state = Start;
+					state = SeparateWs;
 					// append string to arg vector
 				},
 				(StrArg, _) => {
+					let i = cmd.params.len() - 1;
+					if let Param::Arg(ref mut str_arg) = cmd.params[i] {
+						str_arg.push(curr_char);
+					} else {
+						unreachable!();
+					}
 					//push character
 				},
 
@@ -63,41 +142,39 @@ fn tokenize(char_it: &mut Chars) -> (Result<Command, &'static str>, State) { // 
 					unreachable!();
 				},
 			}
+			oldI = i;
 		} else {
 			if state == StrArg {
 				// append string to arg array
 			}
-			break;
+			break;BNF
 		}
 	}
 
-	(Ok(cmd), state)
+	(Ok(cmd), state)*/
 }
 
-fn control_machine(char_it: &mut Chars) -> Result<Command, &'static str> {
+/*fn control_machine(command_line: &str) -> Result<Command, &'static str> {
 	use self::State::*;
 
-	let (res, state) = tokenize(char_it);
+	let (res, state) = tokenize(command_line.char_indices(), command_line);
 
 	if state != Start && state != StrArg {
 		Err("There is an error")
 	} else {
 		res
 	}
-}
+}*/
 
-//TODO:
-//struct Command(Vec<Param>);
-//https://doc.rust-lang.org/book/structs.html
 
-struct Command {
-	cmd: String,
-	params: Vec<Param>,
+struct Command<'a> {
+	cmd: &'a str,
+	params: Vec<Param<'a>>,
 }
-impl Command {
-	fn new() -> Command {
+impl<'a> Command<'a> {
+	fn new(cmd: &str) -> Command {
 		Command {
-			cmd: String::new(),
+			cmd: cmd,
 			params: Vec::new(),
 		}
 	}
@@ -139,9 +216,9 @@ impl Command {
 		}
 	}
 }
-enum Param {
+enum Param<'a> {
 	Arg(String),
-	Cmd(Command),
+	Cmd(Command<'a>),
 
 }
 fn invoke_command(command: &mut Command) {
